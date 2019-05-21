eb89b67f1e239da67d24cd14d7d2c485ff9735ea
Sergiy Mushtyn <smushtyn@gmail.com>
2019-05-21 21:48:19 +0300

    MINOR: warship states skeleton added
    Added <Context> class which is an 'interface' for states
    Added <BaseState> class which maintains link to the context
    Added <BeforeGameState>, <InGameState> and <AfterGameState> classes

---
+++ src/core/context.py
@@ -0,0 +1,17 @@
+from abc import ABC
+"""
+Game state could be changed from the <Context> object as well as from any <BaseState> descendant
+"""
+
+
+class Context(ABC):
+    _state = None
+
+    def __init__(self, state):
+        self.transition_to(state)
+
+    def transition_to(self, state):
+        print(f"[CONTEXT] Transition to {state.__name__}")
+        self._state = state
+        self._state.context = self
+        self._state.whoami()
