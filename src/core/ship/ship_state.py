e904c06195db96b5ba47c0e145fed0b119e04203
Oleksandr Daragan <alexdaran@gmail.com>
2019-06-10 08:50:03 +0300

    added ship skeleton + states + orientation classes. Added common visual class for visual objects

---
+++ src/core/ship/ship_state.py
@@ -0,0 +1,6 @@
+# represents ship states
+class ShipState:
+    unknown: int = 0
+    alive: int = 1
+    injured: int = 2
+    sunk: int = 3
---
+++ src/core/visual.py
@@ -0,0 +1,4 @@
+# represents common class Visual for objects that can be drawn and observe
+class Visual:
+    def __init__(self):
+        pass
