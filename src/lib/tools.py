cd5eef413a5397bc5f8401534921cc19c4abc59b
oabdullin <50902271+oabdullin@users.noreply.github.com>
2019-06-10 16:14:55 +0300

    minor fix for Config (#12)

--- src/lib/tools.py
+++ src/lib/tools.py
@@ -1,11 +1,12 @@
 import yaml
 
 class Config:
+
     def __init__(self, path="lin/config/config.yaml"):
+        self.path = path
         self.data = self.read()
 
     def read(self):
-
         with open(self.path, 'r') as stream:
             try:
                 return yaml.safe_load(stream)
