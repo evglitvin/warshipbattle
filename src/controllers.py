a3eeda1638bcd69ee0874a086aa35569aec10840
nazarenk <ext-volodymyr.nazarenko@here.com>
2019-07-16 12:31:07 +0300

    First commit after code analysis -fix some minor cases

--- src/controllers.py
+++ src/controllers.py
@@ -11,7 +11,7 @@ class UserController:
         pass
 
     def create_opponent(self, bot=True):
-        if bot == True:
+        if bot:
             player = self.create_bot()
         else:
             player = self.create_human()
